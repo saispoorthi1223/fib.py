34f4ac844f7a6d1627af8b55e29295b26d040126
saispoorthi1223 <spoorthibotla@gmail.com>
2025-08-15 21:06:02 +0530

    Add files via upload

---
+++ fib1.py
@@ -0,0 +1,9 @@
+#memorization(top-down)
+def fib_memo(n,memo={}):
+    if n in memo:
+        return memo[n]
+    if n<=1:
+        return n
+    memo[n]=fib_memo(n-1,memo)+fib_memo(n-2,memo)
+    return memo[n]
+print(fib_memo(6))
---
+++ fib2.py
@@ -0,0 +1,10 @@
+#bottom-up(Tabulation)
+def fib_tab(n):
+    if n<=1:
+        return n
+    dp=[0]*(n+1)
+    dp[1]=1
+    for i in range (2,n+1):
+        dp[i]=dp[i-1]+dp[i-2]
+    return dp[n]
+print(fib_tab(6))
